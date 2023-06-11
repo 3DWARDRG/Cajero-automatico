473b575244ebdce8de26b30f9cc6355e4246f544
3DWARDRG <Edwardr185@gmail.com>
2023-06-11 00:14:06 -0500

    empeze a maquetar el proyecto, organizando ideas

---
+++ main.js
@@ -0,0 +1,17 @@
+let image= document.querySelector(".image-start");
+
+image.addEventListener("click", redirection);
+
+setTimeout("redirection()",5000);
+
+
+
+
+
+
+
+
+
+function redirection(){
+    window.location="index2.html";
+}
