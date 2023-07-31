913ef8dd72278703e06aac8d76ac799a4b609c94
EdwardRG <131922277+3DWARDRG@users.noreply.github.com>
2023-07-31 00:24:57 -0500

    Correction AddMoney.js

--- js/AddMoney.js
+++ js/AddMoney.js
@@ -71,7 +71,7 @@ const returnPage= document.querySelector("#returnPage");
 returnPage.addEventListener("click",returnUserTransaction);
 
 function returnUserTransaction(){
-    window.location="UserTransaction.html";
+    window.location="userTransaction.html";
 }
 
 
